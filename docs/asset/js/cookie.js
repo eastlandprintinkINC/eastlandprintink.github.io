c4670997aa9f75c7a497cb76df73cdcf680d1641
Baby Jean Baido <babybaido18@gmail.com>
2022-09-23 11:16:56 +0800

    modified

--- docs/asset/js/cookie.js
+++ docs/asset/js/cookie.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
-    if (sessionStorage.getItem('#cookieConsent') !== 'true') {
-        setTimeout(function () {
-            $("#cookieConsent").fadeIn(200);
-        }, 4000);
-        $("#closeCookieConsent, .cookieConsentOK, #policyClicked").click(function () {
-            $("#cookieConsent").fadeOut(200);
-        }); 
-
-        sessionStorage.setItem('#cookieConsent', 'true');
+    if (getCookie("HideDiv") != "true") {
+        $("#cookieConsent").fadeIn("slow");
+    } else {
+        $("#cookieConsent").css('display', 'none');
     }
-}); 
+});
+
+$(".cookieConsentOK").on('click', function () {
+    setCookie('HideDiv', 'true', 1);
+    $("#cookieConsent").fadeOut("slow");
+})
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setFullYear(d.getFullYear() + 1);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
